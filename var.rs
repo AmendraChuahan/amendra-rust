723a22c30cb122a7f82ba7d671ecfaa808ffad25
Amendra singh <167674924+Amendrar@users.noreply.github.com>
2024-06-24 11:22:46 +0530

    Add files via upload

---
+++ var.rs
@@ -0,0 +1,12 @@
+// variables
+
+
+fn main() {
+    println!("variable");
+    let  mut a= 45;
+    println!(" {a}");
+a  = 43;
+println!("{a}");
+let b= " String i am amendra";
+println!("{b}");
+}
