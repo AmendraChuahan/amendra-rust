58da25b1a84081027a8089e046c0a22f60dff0cc
Amendra singh <167674924+Amendrar@users.noreply.github.com>
2024-06-27 12:12:03 +0530

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,4 +1,52 @@
-//first program
-fn main(){
-    println!(" hello world .");
+fn main() {
+    println!("loops!");
+    //first();
+   // second();
+   //third();
+    fourth();
+}
+// types of loops
+//for, while, loop
+// fn first(){
+//     let mut x = 0;
+
+//     loop {
+//      x += 1;
+//      println!("x = {}",x);
+//      if x == 5{
+//      println!("we did it");
+//      break;
+//     }
+
+// }
+// }
+// fn second(){
+//     let mut number= 0;
+    
+//     while number!=0{
+//         println!("{}",number);
+//           number ==  1;
+//     }
+//     println!(" hello");
+// }
+// fn third(){
+//     let a = [10,20,30,40,50,55];
+//     let mut index=0;
+//     while index<6{// while loop
+//         println!(" the value is {}",a[index]);
+//         index +=1;
+
+//     }
+//     println!(" hello");
+// }
+// for loops
+fn fourth(){
+    for x in 1..11{
+        if x==5 {
+            continue;
+
+        }
+        println!(" the value  {}",  x);
+    }
+   
 }
