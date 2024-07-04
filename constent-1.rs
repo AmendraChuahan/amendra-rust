6154ed5fa8d0730d8962100affb59e2d0ffa23b2
Amendra singh <167674924+Amendrar@users.noreply.github.com>
2024-07-04 12:01:58 +0530

    Add files via upload

---
+++ constent-1.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("constent!");
+    const User_Data:i32=  200;
+    const User_Avg:f64=  45.12;
+    println!("No of user data{User_Data}");
+    println!("avg of user{User_Avg}");
+}
